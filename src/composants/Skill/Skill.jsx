3628b4cbef1a1be8f1f80fc8b9be612aac09b065
cindycs <schaal.cindy@gmail.com>
2024-11-04 11:49:17 +0100

    ajout du contenu "mes competences"

--- src/composants/Skill/Skill.jsx
+++ src/composants/Skill/Skill.jsx
@@ -1,11 +1,27 @@
 import './skill.css';
 import { Link } from "react-router-dom";
+import frontEnd from "../../assets/competences/competences_front.png";
+import backEnd from "../../assets/competences/competences_back.png";
+import framework from "../../assets/competences/competences_framework.png";
 
 export default function Header() {
     return (
-        <section>
-            <p>Mes compétences:</p>
-            <Link to="https://miro.com/app/board/uXjVLNzjaPk=/" target="_blank">Lien vers mes compétences</Link>
+        <>
+        <section className='list-skills'>
+            <div className='skills'>
+                <h3>front-end</h3>
+                <img src={frontEnd} alt="compétences front-end"/>
+            </div>
+            <div className='skills'>
+                <h3>back-end</h3>
+                <img src={backEnd} alt="compétences back-end"/>
+            </div>
+            <div className='skills'>
+                <h3>framework</h3>
+                <img src={framework} alt="compétences framework"/>
+            </div>
         </section>
+        <Link className="link-skills" to="https://miro.com/app/board/uXjVLNzjaPk=/" target="_blank">Lien vers mon mind-map</Link>
+        </>
     )
 }
