62659d1ab76ed43d2195e4c4109103bebbd14f6d
cindycs <schaal.cindy@gmail.com>
2024-11-06 16:40:50 +0100

    effet d'apparition skills

--- src/composants/Skill/Skill.jsx
+++ src/composants/Skill/Skill.jsx
@@ -4,19 +4,19 @@ import frontEnd from "../../assets/competences/competences_front.png";
 import backEnd from "../../assets/competences/competences_back.png";
 import framework from "../../assets/competences/competences_framework.png";
 
-export default function Header() {
+export default function Skill() {
     return (
         <>
         <section className='list-skills'>
-            <div className='skills'>
+            <div className='skills fade-in-up'>
                 <h3>front-end</h3>
                 <img src={frontEnd} alt="compétences front-end"/>
             </div>
-            <div className='skills'>
+            <div className='skills fade-in-up'>
                 <h3>back-end</h3>
                 <img src={backEnd} alt="compétences back-end"/>
             </div>
-            <div className='skills'>
+            <div className='skills fade-in-up'>
                 <h3>framework</h3>
                 <img src={framework} alt="compétences framework"/>
             </div>
