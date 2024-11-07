fa52b6ad217ca41bc1a8915d03254454a4d39c5b
cindycs <schaal.cindy@gmail.com>
2024-11-07 14:32:17 +0100

    actualisation effet parallax Banner

--- src/composants/Banner/Banner.jsx
+++ src/composants/Banner/Banner.jsx
@@ -1,33 +1,34 @@
 import './banner.css';
 import fond from '../../assets/banner/fond.png';
 import nuage from '../../assets/banner/nuage.png';
+import brume from '../../assets/banner/brume.png';
 import soleil from '../../assets/banner/soleil.png'
 import { ParallaxProvider, Parallax } from "react-scroll-parallax";
 
 
 export default function Banner() {
-    /*
-    <section className="banniere">
-                <img src={fond} alt="Description de l'image" />
-                <img src={nuage} alt="Description de l'image" id="cloud"/>
-            </section> 
-    */
+
     return (
         <ParallaxProvider className="bg-parallax">
              <section className="banniere">
-             <img className="bg-banner" src={fond} alt="Description de l'image" />
-
-             <Parallax
-                translateY={[-90, 100]} 
-                className="sun-parallax"
-            >
-             <img src={soleil} alt="Description de l'image" id="soleil"/>
-            </Parallax>
-             <Parallax
-                translateX={[10, -10]} 
-                className="cloud-parallax"
-            >
-                <img src={nuage} alt="Description de l'image" id="cloud"/>
+                <Parallax
+                    translateY={[0, 0]} 
+                    className="sun-parallax"
+                >
+                    <img src={soleil} alt="Description de l'image" id="soleil"/>
+                </Parallax>
+                <img className="bg-banner" src={fond} alt="Description de l'image" />      
+                <Parallax
+                    translateX={[20, -20]} 
+                    className="cloud-parallax"
+                >
+                    <img src={nuage} alt="Description de l'image" id="cloud"/>
+                </Parallax>
+                <Parallax
+                    translateX={[15, -5]} 
+                    className="brume-parallax"
+                >
+                    <img src={brume} alt="Description de l'image" id="brume"/>
                 </Parallax>
             </section> 
             
